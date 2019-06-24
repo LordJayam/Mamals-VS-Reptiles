9885b966295a45b5d7e8fb18785a2c224c87149b
LordJayam <jayam.thaker@gmail.com>
2019-06-24 22:52:36 +0530

    Adding Main File

---
+++ app.py
@@ -0,0 +1,42 @@
+from starlette.applications import Starlette
+from starlette.responses import PlainTextResponse
+from starlette.responses import JSONResponse
+from starlette.templating import Jinja2Templates
+from fastai.vision import *
+import uvicorn
+
+
+app = Starlette()
+app.debug = True
+
+templates = Jinja2Templates(directory='templates')
+
+path = Path("./")
+classes = ["mamals", "reptiles"]
+
+#data = ImageDataBunch.single_from_classes(path,classes,size=224).normalize(imagenet_stats)
+async def init():
+    return load_learner(path,"export.pkl")
+
+as_learn = init()
+
+@app.route('/')
+def index(request):
+    return templates.TemplateResponse("index.html",{'request':request})
+
+@app.route('/submit',methods = ["POST"])
+async def submit(request):
+    learn = await as_learn()
+    data = await request.form()
+    bytes = await data["file"].read()
+    return pred_img_from_bytes(learn,bytes)
+
+
+def pred_img_from_bytes(learn,bytes):
+
+    img = open_image(BytesIO(bytes))
+    losses = learn.predict(img)
+    return  PlainTextResponse(str(losses[0]))
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
